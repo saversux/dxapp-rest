97e2ef74f5dd87040d988e6e10a9ceacc0b9fc28
Julien Bernhart <julien.bernhart@uni-duesseldorf.de>
2018-09-02 15:48:00 +0200

    update for dxram 0.5.0

--- src/main/java/de/hhu/bsinfo/dxapp/rest/RestServerApplication.java
+++ src/main/java/de/hhu/bsinfo/dxapp/rest/RestServerApplication.java
@@ -8,6 +8,7 @@ import de.hhu.bsinfo.dxram.boot.BootService;
 import de.hhu.bsinfo.dxram.chunk.ChunkAnonService;
 import de.hhu.bsinfo.dxram.chunk.ChunkService;
 import de.hhu.bsinfo.dxram.engine.DXRAMVersion;
+import de.hhu.bsinfo.dxram.generated.BuildConfig;
 import de.hhu.bsinfo.dxram.nameservice.NameserviceService;
 import de.hhu.bsinfo.dxram.stats.StatisticsService;
 import org.apache.logging.log4j.Logger;
@@ -25,7 +26,7 @@ public class RestServerApplication extends AbstractApplication {
 
     @Override
     public DXRAMVersion getBuiltAgainstVersion() {
-        return DXRAM.VERSION;
+        return BuildConfig.DXRAM_VERSION;
     }
 
     @Override
