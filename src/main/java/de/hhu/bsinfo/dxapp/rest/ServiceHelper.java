83f0f65ff72c956419bb41bc2abc397a35b53666
Julien Bernhart <julien.bernhart@uni-duesseldorf.de>
2018-09-25 21:47:44 +0200

    add missing service changes

--- src/main/java/de/hhu/bsinfo/dxapp/rest/ServiceHelper.java
+++ src/main/java/de/hhu/bsinfo/dxapp/rest/ServiceHelper.java
@@ -1,8 +1,10 @@
 package de.hhu.bsinfo.dxapp.rest;
 
+import de.hhu.bsinfo.dxapp.rest.cmd.Monitoring;
 import de.hhu.bsinfo.dxram.boot.BootService;
 import de.hhu.bsinfo.dxram.chunk.ChunkAnonService;
 import de.hhu.bsinfo.dxram.chunk.ChunkService;
+import de.hhu.bsinfo.dxram.monitoring.MonitoringService;
 import de.hhu.bsinfo.dxram.nameservice.NameserviceService;
 import de.hhu.bsinfo.dxram.stats.StatisticsService;
 
@@ -12,13 +14,15 @@ public class ServiceHelper {
     public ChunkService chunkService;
     public ChunkAnonService chunkAnonService;
     public StatisticsService statisticsService;
+    public MonitoringService monitoringService;
 
-    public ServiceHelper(BootService bootService, NameserviceService nameService, ChunkService chunkService, ChunkAnonService chunkAnonService, StatisticsService statisticsService) {
+    public ServiceHelper(BootService bootService, NameserviceService nameService, ChunkService chunkService, ChunkAnonService chunkAnonService, StatisticsService statisticsService, MonitoringService monitoringService) {
         this.bootService = bootService;
         this.nameService = nameService;
         this.chunkService = chunkService;
         this.chunkAnonService = chunkAnonService;
         this.statisticsService = statisticsService;
+        this.monitoringService = monitoringService;
 
     }
 
